4afb4770608c1cbe433ba024ddc6579b7c975abf
webad012 <webad012@gmail.com>
2016-02-25 17:47:17 +0100

    added section 5.6;

--- js/QuestionManager.js
+++ js/QuestionManager.js
@@ -22,7 +22,7 @@ var QuestionManager = function() {
                 var question_key = group.id+'_'+subgroup.id+'_'+question.id;
                 if($.inArray(question_key, taken_question_ids) === -1)
                 {
-//                    if(group.id === 5 && subgroup.id === 5)
+//                    if(group.id === 5 && subgroup.id === 6)
 //                    {
 //                        console.log('yes');
 //                    }
