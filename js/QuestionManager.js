0d7cae1bff3a50a4ec37fc6757cbc4fb2499c7ff
webad012 <webad012@gmail.com>
2016-03-01 12:27:15 +0100

    added section 10.0;

--- js/QuestionManager.js
+++ js/QuestionManager.js
@@ -22,7 +22,7 @@ var QuestionManager = function() {
                 var question_key = group.id+'_'+subgroup.id+'_'+question.id;
                 if($.inArray(question_key, taken_question_ids) === -1)
                 {
-//                    if(group.id === 4 && subgroup.id === 9)
+//                    if(group.id === 10 && subgroup.id === 0)
 //                    {
 //                        console.log('yes');
 //                    }
